2e26c82c423f0c7522e4e001b90bb6f5cd80a5af
Abhiram-ops <322506402230@andhrauniversity.edu.in>
2026-02-10 16:10:19 +0530

    bg update and user login

--- Backend/init_db.py
+++ Backend/init_db.py
@@ -93,6 +93,15 @@ def initialize_db():
     )
     """)
 
+    # USERS (Riders)
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+    """)
+
     # ---------------- SAMPLE DATA ----------------
 
     # Routes
